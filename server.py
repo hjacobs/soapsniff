0580b302a633d35234407bab8247914a295ba76d
hjacobs <henning@jacobs1.de>
2013-12-15 15:41:09 +0100

    Code formatting with codevalidator.py

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """
 Statistics server for SOAP/HTTP sniffer.
 Holds request counts.
@@ -12,8 +14,11 @@ import json
 import logging
 from optparse import OptionParser
 
+
 class DerivativeCounter(object):
+
     """calculate derivate (increments/sec) of dictionary counter values"""
+
     def __init__(self, maxlen=60):
         self.maxlen = maxlen
         self.update_interval = 2
@@ -31,26 +36,28 @@ class DerivativeCounter(object):
         else:
             # just update our most recent counter values
             self.current().update(_values)
-    
+
     def current(self):
         """current counter values"""
+
         if not self.values:
             return {}
         return self.values[-1]
 
     def average_per_sec(self, timespan=60):
         """return dictionary with increments/sec for each key"""
+
         if not self.values:
             return {}
         now = self.timestamps[-1]
         if time.time() - now > 2 * self.update_interval:
             # last update was too long ago
-            # => we assume that counters did not change 
+            # => we assume that counters did not change
             # and we calculate average based on current time
             now = time.time() - self.update_interval
         startidx = 0
         dt = now - self.timestamps[startidx]
-        while dt > timespan and startidx < len(self.timestamps)-2:
+        while dt > timespan and startidx < len(self.timestamps) - 2:
             startidx += 1
             dt = now - self.timestamps[startidx]
         if not dt:
@@ -61,9 +68,9 @@ class DerivativeCounter(object):
         for key, val in dv.items():
             dv[key] = (val - startvalues.get(key, 0)) / dt
         return dv
-            
 
-class SOAPCallGrouper(object):
+
+class SoapCallGrouper(object):
 
     @classmethod
     def group(cls, data, by):
@@ -77,35 +84,31 @@ class SOAPCallGrouper(object):
         return result
 
 
-class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class SimpleHttpRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type",'text/plain')
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        separators = (',', ':')
+        separators = ',', ':'
         if 'pretty' in self.path:
-            separators = (',\n', ': ')
+            separators = ',\n', ': '
         if self.path.startswith('/deque'):
             data = list(self.server.get_data())
         elif self.path.startswith('/derivate'):
             if 'soap' in self.path:
                 data = self.server.soap_call_counter.average_per_sec()
                 if 'method' in self.path:
-                    data = SOAPCallGrouper.group(data, 'method')
+                    data = SoapCallGrouper.group(data, 'method')
                 elif 'connection' in self.path:
-                    data = SOAPCallGrouper.group(data, 'connection')
+                    data = SoapCallGrouper.group(data, 'connection')
             elif 'http' in self.path:
                 data = self.server.http_request_counter.average_per_sec()
-            else:    
-                data = {
-                    'http': self.server.http_request_counter.average_per_sec(),
-                    'soap': self.server.soap_call_counter.average_per_sec()
-                }
+            else:
+                data = {'http': self.server.http_request_counter.average_per_sec(),
+                        'soap': self.server.soap_call_counter.average_per_sec()}
         else:
-            data = {
-                'http': self.server.http_request_counter.current(),
-                'soap': self.server.soap_call_counter.current()
-            }
+            data = {'http': self.server.http_request_counter.current(), 'soap': self.server.soap_call_counter.current()}
         json.dump(data, self.wfile, separators=separators)
 
     def do_POST(self):
@@ -119,44 +122,47 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         else:
             self.server.update_counters(data)
         self.send_response(200)
-        self.send_header("Content-type",'text/plain')
-        self.send_header("Content-Length",'2')
+        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-Length', '2')
         self.end_headers()
-        self.wfile.write("OK")
+        self.wfile.write('OK')
 
-class myWebServer(SocketServer.ThreadingMixIn,BaseHTTPServer.HTTPServer):
+
+class MyWebServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
+
     def init(self):
         self.deque = collections.deque([], 500)
         self.http_request_counter = DerivativeCounter()
         self.soap_call_counter = DerivativeCounter()
+
     def append_data(self, data):
         self.deque.append((time.time(), data))
+
     def get_data(self):
         return self.deque
+
     def update_counters(self, data):
         self.http_request_counter.update(data['http'])
         self.soap_call_counter.update(data['soap'])
-            
+
 
 if __name__ == '__main__':
     parser = OptionParser(description=__doc__)
-    parser.add_option("-p", "--port", dest="port",
-        default=8080,
-        help="port to listen on (default: 8080)", metavar="PORT")
-    parser.add_option("-v", "--verbose", dest="verbose",
-        action='store_true',
-        help="output debug information" )
+    parser.add_option('-p', '--port', dest='port', default=8080, help='port to listen on (default: 8080)',
+                      metavar='PORT')
+    parser.add_option('-v', '--verbose', dest='verbose', action='store_true', help='output debug information')
 
     options, args = parser.parse_args()
 
     log_level = logging.INFO
-    if options.verbose: log_level = logging.DEBUG
+    if options.verbose:
+        log_level = logging.DEBUG
     logging.basicConfig(level=log_level)
 
-    server_address = ('',int(options.port))
-    httpd=myWebServer(server_address,SimpleHTTPRequestHandler)
+    server_address = '', int(options.port)
+    httpd = MyWebServer(server_address, SimpleHttpRequestHandler)
     httpd.init()
-    sa=httpd.socket.getsockname()
-    logging.info("Serving HTTP on {0} port {1}...".format(sa[0], sa[1]))
+    sa = httpd.socket.getsockname()
+    logging.info('Serving HTTP on {0} port {1}...'.format(sa[0], sa[1]))
     httpd.serve_forever()
 
--- soapsniff.py
+++ soapsniff.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """
 Simple SOAP/HTTP packet sniffer
 written by henning@jacobs1.de
@@ -23,10 +25,10 @@ from operator import itemgetter
 from heapq import nlargest
 import re
 import xml.etree.ElementTree
-from xml.parsers.expat import ExpatError 
+from xml.parsers.expat import ExpatError
 
 try:
-    import simplejson as json 
+    import simplejson as json
 except ImportError:
     import json
 
@@ -35,11 +37,15 @@ ENVELOPE_END_ANCHORED = re.compile('</[a-zA-Z-]+:Envelope>\s*$')
 NS_SOAP_ENV = '{http://schemas.xmlsoap.org/soap/envelope/}'
 IPV4_ADDRESS = re.compile('^\d+\.\d+\.\d+\.\d+$')
 
+
 class Counter(dict):
+
     def __missing__(self, key):
         return 0
+
     def inc(self, key):
         self[key] = self.get(key, 0) + 1
+
     def update(self, iterable=None, **kwds):
         '''Like dict.update() but add counts instead of replacing them.
 
@@ -52,7 +58,8 @@ class Counter(dict):
         >>> c['h']                      # four 'h' in which, witch, and watch
         4
 
-        '''        
+        '''
+
         if iterable is not None:
             if hasattr(iterable, 'iteritems'):
                 if self:
@@ -60,13 +67,14 @@ class Counter(dict):
                     for elem, count in iterable.iteritems():
                         self[elem] = self_get(elem, 0) + count
                 else:
-                    dict.update(self, iterable) # fast path when counter is empty
+                    dict.update(self, iterable)  # fast path when counter is empty
             else:
                 self_get = self.get
                 for elem in iterable:
                     self[elem] = self_get(elem, 0) + 1
         if kwds:
             self.update(kwds)
+
     def most_common(self, n=None):
         '''List the n most common elements and their counts from the most
         common to the least.  If n is None, then list all element counts.
@@ -74,13 +82,17 @@ class Counter(dict):
         >>> Counter('abracadabra').most_common(3)
         [('a', 5), ('r', 2), ('b', 2)]
 
-        '''        
+        '''
+
         if n is None:
             return sorted(self.iteritems(), key=itemgetter(1), reverse=True)
         return nlargest(n, self.iteritems(), key=itemgetter(1))
 
+
 class Connection(object):
+
     """A TCP connection (identified by pair of sockets)"""
+
     def __init__(self, socket_pair):
         self.src = socket_pair[0]
         self.dst = socket_pair[1]
@@ -99,12 +111,15 @@ class Connection(object):
             last = self.deque.pop()
             self.deque.append((tcp_seq, data))
             self.deque.append(last)
-    
+
     def get_stream(self):
-        return ''.join([dat for s, dat in self.deque])
+        return ''.join([dat for (s, dat) in self.deque])
+
 
 class ConnectionManager(object):
+
     """Container for list of active connections"""
+
     def __init__(self):
         self.connections = {}
 
@@ -113,19 +128,23 @@ class ConnectionManager(object):
 
     def __getitem__(self, socket_pair):
         return self.connections.setdefault(socket_pair, Connection(socket_pair))
-    
+
     def __delitem__(self, socket_pair):
         del self.connections[socket_pair]
-    
+
     def remove_idle(self):
         """remove all connections which did not receive data packets in the last minute"""
+
         deadline = time.time() - 60.0
         for key, conn in self.connections.items():
             if conn.last_packet_received < deadline:
                 del self.connections[key]
 
+
 class ClockThread(Thread):
+
     """simple timer thread which calls a given function periodically"""
+
     def __init__(self, interval, callback):
         self.interval = interval
         self.callback = callback
@@ -136,10 +155,12 @@ class ClockThread(Thread):
         while True:
             time.sleep(self.interval)
             self.callback()
-        
+
 
 class DecoderThread(Thread):
+
     """main thread which handles TCP packets and HTTP/SOAP"""
+
     def __init__(self, pcapObj, server_ports, ws_path, soap_request_handler, server):
         # Query the type of the link and instantiate a decoder accordingly.
         datalink = pcapObj.datalink()
@@ -148,7 +169,7 @@ class DecoderThread(Thread):
         elif pcapy.DLT_LINUX_SLL == datalink:
             self.decoder = LinuxSLLDecoder()
         else:
-            raise Exception("Datalink type not supported: %s" % (datalink,))
+            raise Exception('Datalink type not supported: %s' % (datalink, ))
 
         self.pcap = pcapObj
         self.server_ports = set(server_ports)
@@ -185,7 +206,7 @@ class DecoderThread(Thread):
                 line = fp.readline()
                 i = line.find(';')
                 if i >= 0:
-                    line = line[:i] # strip chunk-extensions
+                    line = line[:i]  # strip chunk-extensions
                 try:
                     chunk_left = int(line, 16)
                 except ValueError:
@@ -195,11 +216,11 @@ class DecoderThread(Thread):
             value.append(fp.read(chunk_left))
 
             # we read the whole chunk, get another
-            fp.read(2)      # toss the CRLF at the end of the chunk
+            fp.read(2)  # toss the CRLF at the end of the chunk
             chunk_left = None
 
         # read and discard trailer up to the CRLF terminator
-        ### note: we shouldn't have any trailers!
+        # ## note: we shouldn't have any trailers!
         while True:
             line = fp.readline()
             if not line:
@@ -209,17 +230,22 @@ class DecoderThread(Thread):
             if line == '\r\n':
                 break
 
-
         return ''.join(value)
 
     def handle_http_request(self, conn, method, data):
         try:
-            request_path = data[len(method)+1:data.index(' ', len(method)+2)]
+            request_path = data[len(method) + 1:data.index(' ', len(method) + 2)]
         except ValueError:
             logging.warning('Invalid HTTP request line: %s', data[:100])
             return
         logging.debug('Received %s %s', method, request_path)
-        self.http_request_counter.inc((conn.src[0], conn.dst[0], conn.dst[1], method, request_path))
+        self.http_request_counter.inc((
+            conn.src[0],
+            conn.dst[0],
+            conn.dst[1],
+            method,
+            request_path,
+        ))
         self._needs_server_update = True
 
         # SOAP requests must be POST
@@ -234,12 +260,12 @@ class DecoderThread(Thread):
         first = True
         while conn.deque:
             seq, packet = conn.deque.popleft()
-            if not first and packet.startswith('POST /'): 
+            if not first and packet.startswith('POST /'):
                 conn.deque.append((seq, packet))
-                break;
-            chunk += packet  
+                break
+            chunk += packet
             first = False
-        #if not conn.deque:
+        # if not conn.deque:
         #    del self.connections[conn]
         fd = StringIO.StringIO(chunk)
         encoding_chunked = False
@@ -249,10 +275,10 @@ class DecoderThread(Thread):
         x_forwarded_for = '-'
         while True:
             line = fd.readline().strip()
-            if first: 
+            if first:
                 firstline = line
                 first = False
-            else: 
+            else:
                 headers.append(line)
                 name, sep, val = line.partition(':')
                 if name.lower() == 'x-forwarded-for' and IPV4_ADDRESS.match(val.strip()):
@@ -277,28 +303,35 @@ class DecoderThread(Thread):
             body = envelope.find(NS_SOAP_ENV + 'Body')
             body_child = None
             for child in body:
-                namespace_uri, method = string.split(child.tag[1:], "}", 1)
+                namespace_uri, method = string.split(child.tag[1:], '}', 1)
                 body_child = child
             endpoint = firstline.split(' ')[1]
-            self.soap_call_counter.inc((x_forwarded_for, conn.src[0], conn.dst[0], conn.dst[1], endpoint, method))
+            self.soap_call_counter.inc((
+                x_forwarded_for,
+                conn.src[0],
+                conn.dst[0],
+                conn.dst[1],
+                endpoint,
+                method,
+            ))
             self._needs_server_update = True
             self.soap_request_handler(conn, firstline.split(' ')[1], headers, payload, body_child)
-        
+
     def update_server(self):
-        logging.debug('Updating server')        
+        logging.debug('Updating server')
         post_data = {'soap': {}, 'http': {}}
         for key, val in self.http_request_counter.items():
             post_data['http'][' '.join(map(str, key))] = val
         for key, val in self.soap_call_counter.items():
             post_data['soap'][' '.join(map(str, key))] = val
         try:
-            self.server_conn.request("POST", "/counter", json.dumps(post_data))
+            self.server_conn.request('POST', '/counter', json.dumps(post_data))
             response = self.server_conn.getresponse()
             response.read()
-        except Exception, e:
+        except:
             logging.exception('Error while posting to server')
             self._init_server_conn()
-            
+
         self._last_server_update = time.time()
         self._needs_server_update = False
 
@@ -308,6 +341,7 @@ class DecoderThread(Thread):
 
     def handle_packet(self, hdr, data):
         """pcap packet handler"""
+
         # Use the ImpactDecoder to turn the rawpacket into a hierarchy
         # of ImpactPacket instances.
         a = self.decoder.decode(data)
@@ -320,31 +354,32 @@ class DecoderThread(Thread):
 
         try:
             self._handle_tcp_packet(ip, tcp)
-        except Exception, e:
+        except:
             logging.exception('Error while processing TCP packet: %s', tcp)
 
     def _handle_tcp_packet(self, ip, tcp):
         """handle a single TCP/IP packet"""
+
         self.packet_counter += 1
         if self.packet_counter % 10 == 0:
             logging.debug('Received %d packets so far', self.packet_counter)
 
-        src = (ip.get_ip_src(), tcp.get_th_sport() )
-        dst = (ip.get_ip_dst(), tcp.get_th_dport() )
-        socket_pair = (src, dst)
+        src = ip.get_ip_src(), tcp.get_th_sport()
+        dst = ip.get_ip_dst(), tcp.get_th_dport()
+        socket_pair = src, dst
 
         if self.packet_counter % 10 == 0:
             self.connections.remove_idle()
 
         conn = self.connections[socket_pair]
-            
+
         logging.debug('Connections: %d', len(self.connections))
 
         data = tcp.get_data_as_string()
         conn.append_packet(tcp.get_th_seq(), data)
 
         try:
-            http_method = data[:data.index(' ', self._shortest_method_name, self._longest_method_name+1)]
+            http_method = data[:data.index(' ', self._shortest_method_name, self._longest_method_name + 1)]
         except ValueError:
             # HTTP method could not be parsed => probably a data packet
             http_method = None
@@ -354,8 +389,10 @@ class DecoderThread(Thread):
 
         if conn.is_soap and ENVELOPE_END.search(conn.get_stream(), 20):
             self.handle_soap_request(conn)
-        
+
+
 class Watcher:
+
     """this class solves two problems with multithreaded
     programs in Python, (1) a signal might be delivered
     to any thread (which is just a malfeature) and (2) if
@@ -370,12 +407,13 @@ class Watcher:
     I have only tested this on Linux.  I would expect it to
     work on the Macintosh and not work on Windows.
     """
-    
+
     def __init__(self):
         """ Creates a child thread, which returns.  The parent
             thread waits for a KeyboardInterrupt and then kills
             the child thread.
         """
+
         self.child = os.fork()
         if self.child == 0:
             return
@@ -395,14 +433,18 @@ class Watcher:
     def kill(self):
         try:
             os.kill(self.child, signal.SIGKILL)
-        except OSError: pass
+        except OSError:
+            pass
+
 
 def default_soap_request_handler(conn, endpoint, headers, payload, body_child):
-    namespace_uri, tag = string.split(body_child.tag[1:], "}", 1)
-    logging.debug("SOAP %s %s (%s)", endpoint, tag, conn)
+    namespace_uri, tag = string.split(body_child.tag[1:], '}', 1)
+    logging.debug('SOAP %s %s (%s)', endpoint, tag, conn)
+
 
 def main(dev, server_ports, filter, ws_path, soap_request_handler, server):
     """main function which starts up the decoder thread"""
+
     Watcher()
 
     # Open interface for catpuring.
@@ -411,7 +453,7 @@ def main(dev, server_ports, filter, ws_path, soap_request_handler, server):
     # Set the BPF filter. See tcpdump(3).
     p.setfilter(filter)
 
-    logging.info("Listening on %s: net=%s, mask=%s, linktype=%d" % (dev, p.getnet(), p.getmask(), p.datalink()))
+    logging.info('Listening on %s: net=%s, mask=%s, linktype=%d' % (dev, p.getnet(), p.getmask(), p.datalink()))
 
     # Start sniffing thread and finish main thread.
     t = DecoderThread(p, server_ports, ws_path, soap_request_handler, server)
@@ -423,41 +465,36 @@ def main(dev, server_ports, filter, ws_path, soap_request_handler, server):
 if __name__ == '__main__':
 
     parser = OptionParser(description=__doc__)
-    parser.add_option("-d", "--dev", dest="dev",
-        default="eth0",
-        help="device to listen on (default: eth0)", metavar="DEV")
-    parser.add_option("-p", "--ports", dest="ports",
-        default="80,8080",
-        help="ports to listen on (default: 80,8080)", metavar="PORTS")
-    parser.add_option("-w", "--ws-path", dest="ws_path", metavar="PREFIX",
-        default="/",
-        help="web service path prefix (e.g. /ws)" )
-    parser.add_option("-c", "--soap-request-handler", dest="soap_request_handler", metavar="CALLABLE",
-        help="soap request handler callback" )
-    parser.add_option("-s", "--server", dest="server", metavar="HOST_PORT",
-        help="server host and port" )
-    parser.add_option("-v", "--verbose", dest="verbose",
-        action='store_true',
-        help="output debug information" )
+    parser.add_option('-d', '--dev', dest='dev', default='eth0', help='device to listen on (default: eth0)',
+                      metavar='DEV')
+    parser.add_option('-p', '--ports', dest='ports', default='80,8080', help='ports to listen on (default: 80,8080)',
+                      metavar='PORTS')
+    parser.add_option('-w', '--ws-path', dest='ws_path', metavar='PREFIX', default='/',
+                      help='web service path prefix (e.g. /ws)')
+    parser.add_option('-c', '--soap-request-handler', dest='soap_request_handler', metavar='CALLABLE',
+                      help='soap request handler callback')
+    parser.add_option('-s', '--server', dest='server', metavar='HOST_PORT', help='server host and port')
+    parser.add_option('-v', '--verbose', dest='verbose', action='store_true', help='output debug information')
 
     options, args = parser.parse_args()
 
     log_level = logging.INFO
-    if options.verbose: log_level = logging.DEBUG
+    if options.verbose:
+        log_level = logging.DEBUG
     logging.basicConfig(level=log_level)
 
     ports = []
     ports_filter = ''
-    
+
     if options.ports:
         parts = options.ports.split(',')
         for part in parts:
             range_start_end = part.split('-')
             if len(range_start_end) == 2:
-                ports += range(int(range_start_end[0]), int(range_start_end[1])+1)
+                ports += range(int(range_start_end[0]), int(range_start_end[1]) + 1)
             else:
                 ports.append(int(range_start_end[0]))
-        ports_filter = ' or '.join(['(port %s)' % (p,) for p in ports])
+        ports_filter = ' or '.join(['(port %s)' % (p, ) for p in ports])
         ports_filter = ' and (' + ports_filter + ')'
 
     # we only capture packets which contain data:
@@ -465,7 +502,7 @@ if __name__ == '__main__':
     # (ip[0]&0xf)<<2 : 4-bit IP header length
     # (tcp[12]&0xf0) : TCP header length
     filter = 'tcp ' + ports_filter + ' and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)'
-    logging.debug("BPF filter: %s", filter)
+    logging.debug('BPF filter: %s', filter)
 
     if options.soap_request_handler:
         parts = options.soap_request_handler.split('.')
@@ -477,5 +514,4 @@ if __name__ == '__main__':
         soap_request_handler = default_soap_request_handler
 
     main(options.dev, ports, filter, options.ws_path, soap_request_handler, options.server)
-
 
